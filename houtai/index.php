c907ed530cc79267e8f892fae61d994074edbe15
qaz261733100 <245868131@qq.com>
2018-12-24 21:41:57 +0800

    houtai

--- houtai/index.php
+++ houtai/index.php
@@ -12,14 +12,14 @@
 <?php
 include ('link.php');
 ?>
-    <form method="post" action="yaoqiu.php">
-        <?php
-        $sql=mysqli_query($conn,"select * from 后台管理 where id='征婚要求'");
-        $info=mysqli_fetch_array($sql);
-        ?>
-        <label for="myName">征婚要求----------</label>
-        <input type="text" name="text3" class="xiao" value=" <?php echo $info["修改内容"]; ?>">
-        <input type="submit" value="确认更改" onclick="1" class="quer"><br/>
-    </form>    
+<form method="post" action="yaoqiu.php">
+    <?php
+    $sql=mysqli_query($conn,"select * from 后台管理 where id='征婚要求'");
+    $info=mysqli_fetch_array($sql);
+    ?>
+    <label for="myName">征婚要求----------</label>
+    <input type="text" name="text3" class="xiao" value=" <?php echo $info["修改内容"]; ?>">
+    <input type="submit" value="确认更改" onclick="1" class="quer"><br/>
+</form>
 </body>
 </html>>
