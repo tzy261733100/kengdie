ed36cf91697ce881cdd603c664c2af1760226494
qaz261733100 <245868131@qq.com>
2019-02-14 18:14:40 +0800

    all

--- houtai/yaoqiu.php
+++
@@ -1,7 +0,0 @@
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<?php
-$text=$_POST["text"];
-include ("link.php");
-mysqli_query($conn,"update houtai set 修改内容='$text' where id= '征婚要求'");
-echo "<script>alert('修改成功');history.back();</script>";
-?>
--- zuihou/精小弘在线.php
+++
@@ -1,7 +0,0 @@
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<?php
-$text=$_POST["text4"];
-include ("conn.php");
-mysqli_query($conn,"update 后台管理 set 修改内容='$text' where id= '精小弘在线'");
-echo "<script>alert('修改成功');history.back();</script>";
-?>
